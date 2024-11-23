e515f957c5dcc753e55e6c91f31a2a18871949e9
CarlosHuamanM <thecarlox2000@gmail.com>
2024-11-23 18:01:07 -0500

    Interfaz de Agregar Horarios

---
+++ src/app/pages/agregarhorario/agregarhorario.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CalendarComponent } from "../../core/components/calendar/calendar.component";
+
+@Component({
+  selector: 'app-agregarhorario',
+  standalone: true,
+  imports: [CalendarComponent],
+  templateUrl: './agregarhorario.component.html',
+  styleUrl: './agregarhorario.component.css'
+})
+export class AgregarhorarioComponent {
+
+}
